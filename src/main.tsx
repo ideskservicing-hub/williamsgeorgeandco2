1a71baa4967af4f9f043b204bfc34b07e4616b1a
ideskservicing <ideskservicing@gmail.com>
2026-03-25 11:42:39 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+console.log('Main.tsx is running');
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  console.error('Root element not found');
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
